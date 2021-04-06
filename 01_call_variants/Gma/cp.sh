670563e5e78d7c6c2bf15235f3fdf0ec50d8519b
Alejandro Llanos Garrido <allanosgarrido@holy7c24302.rc.fas.harvard.edu>
2021-04-06 11:08:55 -0400

    adding_cp

---
+++ 01_call_variants/Gma/cp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -p shared # partition (queue)
+#SBATCH -N 1 # number of nodes
+#SBATCH --mem=1G #Memory requested
+#SBATCH -t 3-00:00 # time (D-HH:MM)
+#SBATCH -o cp.%j.out # STDOUT
+#SBATCH -e cp.%j.err # STDERR
+
+cp -R --preserve=links /n/holylfs/LABS/informatics/alex_lg/cod-pop-gen/01_call_variants/Gma/sm/gatk ./sm/gatk
