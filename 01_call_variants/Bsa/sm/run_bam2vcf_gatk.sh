dd9d1e5814f4e787aa46c4a4ad13ffcb3584a2de
Alejandro Llanos Garrido <allanosgarrido@boslogin02.rc.fas.harvard.edu>
2021-04-15 12:21:03 -0400

    module_add

--- 01_call_variants/Bsa/sm/run_bam2vcf_gatk.sh
+++ 01_call_variants/Bsa/sm/run_bam2vcf_gatk.sh
@@ -2,12 +2,12 @@
 #SBATCH -J sm
 #SBATCH -o out
 #SBATCH -e err
-#SBATCH -p shared
+#SBATCH -p holy-info
 #SBATCH -n 1
-#SBATCH -t 9000
+#SBATCH -t 30-00:00:00
 #SBATCH --mem=10000
 
-
+module load Anaconda3/2020.11
 source activate snakemake
 snakemake --snakefile Snakefile_bam2vcf_gatk --profile ./profiles/slurm
 
